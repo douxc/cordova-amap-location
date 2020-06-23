929251ba608bb53082ab3dab8d3bf5ac9424e367
douxc <douxc512@gmail.com>
2020-06-23 23:09:55 +0800

    定位sdk升级到5.0，尝试后台定位服务

--- src/android/Location.java
+++ src/android/Location.java
@@ -23,6 +23,47 @@ import android.content.pm.PackageManager;
 import android.util.Log;
 
 public class Location extends CordovaPlugin implements AMapLocationListener{
+    private static final String NOTIFICATION_CHANNEL_NAME = "BackgroundLocation";
+private NotificationManager notificationManager = null;
+boolean isCreateChannel = false;
+@SuppressLint("NewApi")
+private Notification buildNotification() {
+
+	Notification.Builder builder = null;
+	Notification notification = null;
+	if(android.os.Build.VERSION.SDK_INT >= 26) {
+		//Android O上对Notification进行了修改，如果设置的targetSDKVersion>=26建议使用此种方式创建通知栏
+		if (null == notificationManager) {
+			notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		}
+		String channelId = getPackageName();
+		if(!isCreateChannel) {
+			NotificationChannel notificationChannel = new NotificationChannel(channelId,
+					NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+			notificationChannel.enableLights(true);//是否在桌面icon右上角展示小圆点
+			notificationChannel.setLightColor(Color.BLUE); //小圆点颜色
+			notificationChannel.setShowBadge(true); //是否在久按桌面图标时显示此渠道的通知
+			notificationManager.createNotificationChannel(notificationChannel);
+			isCreateChannel = true;
+		}
+		builder = new Notification.Builder(getApplicationContext(), channelId);
+	} else {
+		builder = new Notification.Builder(getApplicationContext());
+	}
+	builder.setSmallIcon(R.drawable.ic_launcher)
+			.setContentTitle(Utils.getAppName(this))
+			.setContentText("正在后台运行")
+			.setWhen(System.currentTimeMillis());
+
+	if (android.os.Build.VERSION.SDK_INT >= 16) {
+		notification = builder.build();
+	} else {
+		return builder.getNotification();
+	}
+	return notification;
+}
+
+
     
     String TAG = "GeolocationPlugin";
     String [] permissions = { Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION };
@@ -58,6 +99,7 @@ public class Location extends CordovaPlugin implements AMapLocationListener{
              keepLocationInstance.onDestroy();
              keepLocationInstance = null;
             }
+            locationClient.enableBackgroundLocation(2001,buildNotification())
             
             int interval = args.optInt(0, 10000); //获取定位间隔参数，缺省10秒钟定位一次
             
@@ -84,6 +126,7 @@ public class Location extends CordovaPlugin implements AMapLocationListener{
                 keepLocationInstance.onDestroy();
                 keepLocationInstance = null;
             }
+            locationClient.disableBackgroundLocation(true);
             callback.success();
             return true;
         } else {
@@ -179,4 +222,5 @@ public class Location extends CordovaPlugin implements AMapLocationListener{
     {
         PermissionHelper.requestPermissions(this, requestCode, permissions);
     }
+
 }
